a62c1dd22a531d36ba48a07f916a697a631b13ab
zichen ma <zma@fundopolis.com>
2020-01-26 12:36:00 -0500

    added, config

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,8 @@ module.exports = {
     ],
     "setupFilesAfterEnv": [
       "<rootDir>/src/setupTests.js",
-      "./node_modules/jest-enzyme/lib/index.js"
+      "./node_modules/jest-enzyme/lib/index.js",
+      "<rootDir>/src/utils/testSetup.js",
     ],
     "testMatch": [
       "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
