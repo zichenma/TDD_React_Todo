9710f5c958dc1b2f519ba6329aa263531260ed70
zichen ma <zma@fundopolis.com>
2020-01-28 10:30:43 -0500

    Finished

---
+++ src/containers/TodoList/__tests__/integration/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TodoList from '../../index';
+import { findTestWrapper } from '../../../../utils/testUtils';
+
+
+it(`
+    1. 用户会在header输入框输入内容
+    2. 用户会点击回车按钮
+    3. 列表项应该怎家用户输入内容的列表项
+`, () => {
+    const wrapper = mount(<TodoList />);
+    const inputElem = findTestWrapper(wrapper, 'input');
+    const content = 'Dell lee';
+    wrapper.setState({value : content });
+    inputElem.simulate('change', {
+        target: { value: content}
+    });
+    inputElem.simulate('keyUp', {
+        keyCode: 13 // Enter key
+    });
+    const listItems = findTestWrapper(wrapper, 'list-item');
+    expect(listItems.length).toBe(1);
+    expect(listItems.at(0).text()).toContain(content);
+})
+
+
